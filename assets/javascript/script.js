543b03a86c84ab07cbfb102fd218e40288387707
saschaschneider <s4schnei@gmail.com>
2017-10-01 16:15:39 -0700

    finished basic functionalities

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -16,47 +16,147 @@
 
   var database = firebase.database();
 
+var buttonpressedP1 = false;
+var buttonpressedP2 = false;
 
 $("#myFormP1, #myFormP2").submit(function username(e) {
-    e.preventDefault()
+            e.preventDefault()
 
-    playerName1 = $(".userNameP1").val().trim();
-    playerName2 = $(".userNameP2").val().trim();
+
+
+            playerName1 = $(".userNameP1").val().trim();
+            playerName2 = $(".userNameP2").val().trim();
+            
+            console.log("Did we press ButtonP1 Start= " +playerName1.length);
+            console.log("Did we press ButtonP2 Start= " +playerName2.length);
+
+            if (playerName1.length > 0) {
+              buttonpressedP1 = true;
+              $(".fieldsetP1").attr("disabled", "disabled"); //disabling the P1 form, so no additional input can be added
+              $(".nameP1").html("Welcome "+playerName1+ ", you are Player 1"); // Adding personal greating message
+              $(".userNameP1").attr("placeholder","Waiting for PLayer 2");
+
+            } 
+            
+            if (playerName2.length > 0) {
+              buttonpressedP2 = true;
+              $(".fieldsetP2").attr("disabled", "disabled"); //disabling the P1 form, so no additional input can be added
+              $(".nameP2").html("Welcome "+playerName2+ ", you are Player 2"); // Adding personal greating message
+
+              }
+             
+
+            if (buttonpressedP1 === true && buttonpressedP2 === true){
+
+             var pushedRef = firebase.database().ref().push({
+                 username1: playerName1,
+                 username2: playerName2,
+                 dateAdded: firebase.database.ServerValue.TIMESTAMP
+
+           })
+          $(".userNameP1").val(""); // clearing the username the user put in the form
+          $(".userNameP2").val(""); // clearing the username the user put in the form
+
+           }
+
+      })
+
+
+var choiceP1Pressed = false;
+var choiceP2Pressed = false;
+var choiceP1;
+var choiceP2;    
+$(".cardgroupP1, .cardgroupP2").on('click', ".btn", function button() {
+
+
+    if ($(this).attr("class") === "btn btn-primary p1") {
+        choiceP1 = $(this).val();
+        // firebase.database().ref().push({
+        // choiceP1: choiceP1  });
+        choiceP1Pressed = true;
+        console.log("P1 selected = " + choiceP1);
+
+    } if ($(this).attr("class") === "btn btn-primary p2") {
+
+        choiceP2 = $(this).val();
+        console.log("P2 selected = " + choiceP2);
+        choiceP2Pressed = true;
+        // firebase.database().ref().push({
+        // choiceP2: choiceP2  });
+    }
+
+  if (choiceP1Pressed === true && choiceP2Pressed === true)
+                 {
+                  console.log("P1 selected End= " + choiceP1);
+                  console.log("P2 selected End= " + choiceP2);
+              if (
+                  (choiceP1 === "Rock" && choiceP2 === "Scissors") ||
+                  (choiceP1 === "Paper") && (choiceP2 === "Rock") ||
+                  (choiceP1 === "Scissors") && (choiceP2 === "Paper"))
+                  { 
+                  console.log("P1 wins");
+                  $(".outcome-text").html("Player 1 Wins") 
+
+                  } 
+                  else if (choiceP1 === choiceP2)
+                  {
+                  console.log("we tie"); // tie conditions
+                  $(".outcome-text").html("This is a tie")
+                  }
+                  else
+                  {
+                  console.log("p2 wins"); // losing condition
+                  $(".outcome-text").html("PLayer 2 wins")
+                  }
+   }
+   else 
+   {
+   setTimeout(button,250); 
+   }})
+
     
 
-   firebase.database().ref().push({
-    username1: playerName1,
-    username2: playerName2,
-    dateAdded: firebase.database.ServerValue.TIMESTAMP
 
-  });
 
-     database.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", function(snapshot) {
-      // storing the snapshot.val() in a variable for convenience
-      var sv = snapshot.val();
+
+  //   console.log("pushedRef = " +pushedRef.key);
+  //   var url123 = "https://rps-multiplayer-e165f.firebaseio.com/" +pushedRef.key; 
+  //   console.log("pushedRefURL = " +url123);
+
+  // var db = firebase.database();
+  // console.log("firebase.database =" + firebase.database());
+  // // db.ref(+pushedRef.key).update({username2: "Player2" });
+  // db.ref(pushedRef.key).update({username2: "LatestTest"});
+  
+   // var pushedRef = firebase.database().ref('/customers').push({ email: email });
+   // console.log("pushedRef = " +pushedRef.key);
+
+
+  
+
+
+// // winning condition for computer
+//         if ((choiceP1==="Rock" && choiceP2==="Scissors") ||
+//            (choiceP1==="Paper") && (choiceP2==="Rock") ||
+//            (choiceP1==="Scissors") && (choiceP2==="Paper")) {
+//           console.log("P1 wins");
+//           $(".outcome-text").html("Computer Wins")
+
+//         } 
+
+//         else if (choiceP1===choiceP2) {
+//           console.log("we tie"); // tie conditions
+//           $(".outcome-text").html("This is a tie")
+//         } 
+
+//         else  {
+//           console.log("p2 wins"); // losing condition
+//           $(".outcome-text").html("PLayer 1 wins")
+//         }
+//     })
     
 
-      if (sv.username1.length > 0 ) {
-        $(".userNameP1").val(""); // clearing the username the user put in the form
-      $(".fieldsetP1").attr("disabled", "disabled"); //disabling the P1 form, so no additional input can be added
-      $(".nameP1").html("Welcome "+sv.username1+ ", you are Player 1"); // Adding personal greating message
-      }
-      else if (sv.username2.length > 0 ) {
-        $(".userNameP2").val(""); // clearing the username the user put in the form
-      $(".fieldsetP2").attr("disabled", "disabled"); //disabling the P1 form, so no additional input can be added
-      $(".nameP2").html("Welcome "+sv.username2+ ", you are Player 2"); // Adding personal greating message
-      }
 
-   })
-    
-})
-
-
-
-
-
-
-    
 
 
 
@@ -77,60 +177,60 @@ $("#myFormP1, #myFormP2").submit(function username(e) {
 
   // working RPS Code - don't touch
 
-   document.onkeyup = function(event){
-      storeChoice = event.key;
-      console.log("store choice" + " " + storeChoice);
+//    document.onkeyup = function(event){
+//       storeChoice = event.key;
+//       console.log("store choice" + " " + storeChoice);
 
-       if (storeChoice==="r") {
-          ourChoice="rock";
-          $(".player1-text").html("Rock")
-        } else if (storeChoice==="p") {
-          ourChoice="paper";
-          $(".player1-text").html("Paper")
-        } else if (storeChoice==="s") {
-          ourChoice="scissors";
-          $(".player1-text").html("Scissors")
-          }
+//        if (storeChoice==="r") {
+//           ourChoice="rock";
+//           $(".player1-text").html("Rock")
+//         } else if (storeChoice==="p") {
+//           ourChoice="paper";
+//           $(".player1-text").html("Paper")
+//         } else if (storeChoice==="s") {
+//           ourChoice="scissors";
+//           $(".player1-text").html("Scissors")
+//           }
 
-        console.log("ourChoice" + " " +ourChoice);
+//         console.log("ourChoice" + " " +ourChoice);
 
-       var gameOptions=["rock","paper","scissors"];
+//        var gameOptions=["rock","paper","scissors"];
 
-// randomizer for computer options
-        var computerChoice=gameOptions[Math.floor(Math.random() * 3)];
-        console.log("computer choice " + computerChoice);
-        // var ourChoice=gameOptions[Math.floor(Math.random() * 3)];
-        // console.log("our choice " + ourChoice);
+// // randomizer for computer options
+//         var computerChoice=gameOptions[Math.floor(Math.random() * 3)];
+//         console.log("computer choice " + computerChoice);
+//         // var ourChoice=gameOptions[Math.floor(Math.random() * 3)];
+//         // console.log("our choice " + ourChoice);
 
-        if (computerChoice==="rock") {
-          computerChoice="rock";
-          $(".player2-text").html("Rock")
-        } else if (computerChoice==="paper") {
-          computerChoice="paper";
-          $(".player2-text").html("Paper")
-        } else if (computerChoice==="scissors") {
-          computerChoice="scissors";
-          $(".player2-text").html("Scissors")
-          }
+//         if (computerChoice==="rock") {
+//           computerChoice="rock";
+//           $(".player2-text").html("Rock")
+//         } else if (computerChoice==="paper") {
+//           computerChoice="paper";
+//           $(".player2-text").html("Paper")
+//         } else if (computerChoice==="scissors") {
+//           computerChoice="scissors";
+//           $(".player2-text").html("Scissors")
+//           }
 
 
-// winning condition for computer
-        if ((computerChoice==="rock" && ourChoice==="scissors") ||
-           (computerChoice==="paper") && (ourChoice==="rock") ||
-           (computerChoice==="scissors") && (ourChoice==="paper")) {
-          console.log("computer wins");
-          $(".outcome-text").html("Computer Wins")
+// // winning condition for computer
+//         if ((computerChoice==="rock" && ourChoice==="scissors") ||
+//            (computerChoice==="paper") && (ourChoice==="rock") ||
+//            (computerChoice==="scissors") && (ourChoice==="paper")) {
+//           console.log("computer wins");
+//           $(".outcome-text").html("Computer Wins")
 
-        } 
+//         } 
 
-        else if (computerChoice===ourChoice) {
-          console.log("we tie"); // tie conditions
-          $(".outcome-text").html("This is a tie")
-        } 
+//         else if (computerChoice===ourChoice) {
+//           console.log("we tie"); // tie conditions
+//           $(".outcome-text").html("This is a tie")
+//         } 
 
-        else  {
-          console.log("we win"); // losing condition
-          $(".outcome-text").html("PLayer 1 wins")
-        }
-    }
-    
+//         else  {
+//           console.log("we win"); // losing condition
+//           $(".outcome-text").html("PLayer 1 wins")
+//         }
+//     }
+//     
